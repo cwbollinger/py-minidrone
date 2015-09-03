08fe3c0fba2950f970284040c8f95690858863c7
cwbollinger <cwbollinger@gmail.com>
2015-09-03 15:26:19 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#! /bin/env bash
+
+# This script installs all the prereqs for the py-minidrone software on an ubuntu live cd
+
+sudo pip install pexpect
+sudo apt-get install libglib2.0
+sudo apt-get install libdbus-1-dev
+sudo apt-get install libudev-dev
+sudo apt-get install libical-dev
+sudo apt-get install libreadline-dev
+download/build/install bluez 5
